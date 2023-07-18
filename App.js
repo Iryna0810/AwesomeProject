7b8b4c44e333748052da2e0912aac203162a59db
Rew <irinavakulenko2000@gmail.com>
2023-07-18 23:57:48 +0300

    start

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+
 
 export default function App() {
+  //   const [fontsLoaded] = useFonts({
+  //   'Inter-Black': require('./assets/fonts/Inter-Black.otf'),
+  // });
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
