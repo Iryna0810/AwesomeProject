a4dac86346600462139f271de1606800a380c365
Rew <irinavakulenko2000@gmail.com>
2023-08-10 01:50:42 +0300

    form

--- Screens/CreatePostsScreen.jsx
+++ Screens/CreatePostsScreen.jsx
@@ -1,28 +1,86 @@
-import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useState } from "react";
+import { View, TextInput, StyleSheet, Button } from "react-native";
 import { useNavigation, useRoute } from '@react-navigation/native';
+import { Formik } from 'formik';
+ import { nanoid } from "nanoid";
 
 
 const CreatePosts = () => {
-const navigation = useNavigation();
-// const { params: { email } } = useRoute();
+  const navigation = useNavigation();
+  const [article, setArticles] = useState([]);
 
+  const addArticles = (article) => {
+    setArticles((list) => {
+      return [article,
+        ...list]
+    })
+  }
 
   return (
     <View style={styles.container}>
-      <Text>Публікації</Text>
-      <Text>Email </Text>
-      <Button title="Go to Profile" onPress={() => navigation.navigate("Profile")} />
+      <Formik
+        initialValues={{
+          image: "",
+          name: '',
+          location: '',
+          key: nanoid(),
+
+        }}
+        onSubmit={values => {
+          addArticles(values);
+          resetForm();
+        }}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values }) => (
+          <View>
+            <TextInput style={styles.textInput}
+              onChangeText={handleChange('image')}
+              onBlur={handleBlur('image')}
+              placeholder="Завантажте зображення"
+              value={values.image}
+            />
+            <TextInput style={styles.textInput}
+              onChangeText={handleChange('name')}
+              onBlur={handleBlur('name')}
+              placeholder="Назва"
+              value={values.name}
+            />
+            <TextInput style={styles.textInput}
+              onChangeText={handleChange('location')}
+              onBlur={handleBlur('location')}
+              placeholder="Місцевість"
+              value={values.location}
+            />
+            <Button style={styles.button}
+              onPress={handleSubmit}
+              title="Submit" />
+          </View>
+          
+        )}
+        
+      </Formik>
     </View>
   );
 };
+
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    paddingHorizontal: 16,
   },
+  textInput: {
+    width: 343,
+    color: '#BDBDBD',
+    borderColor: '#E8E8E8',
+    marginBottom: 16,
+    borderBottomColor: '#E8E8E8',
+
+  }
 });
 
 export default CreatePosts;
--- Screens/HomeTab.jsx
+++ Screens/HomeTab.jsx
@@ -8,7 +8,8 @@ import { Button, View, Text, StyleSheet } from "react-native";
 import { useNavigation, useRoute } from '@react-navigation/native';
 import CommentsScreen from "./CommentsScreen";
 import Profile from "./ProfileScreen";
-import { Ionicons } from "@expo/vector-icons";
+import { Feather, Ionicons } from "@expo/vector-icons";
+import { MaterialIcons } from '@expo/vector-icons'; 
 import PostsScreen from './PostsScreen';
 import Logout from '../images/log-out.svg';
 
@@ -21,24 +22,31 @@ export default function Home() {
 
   return(
     <HomeTab.Navigator initialRouteName='PostScreen'
+    style={styles.container}  
       screenOptions={({ route }) => ({
         tabBarIcon: ({ focused, color, size }) => {
           let iconName;
 
           if (route.name === 'PostScreen') {
             iconName = focused
-              ? 'ios-information-circle'
-              : 'ios-information-circle-outline';
-          } else if (route.name === 'CreatePosts' || route.name === 'Comments' || route.name === 'Profile') {
-            iconName = focused ? 'ios-list' : 'ios-list-outline';
+              ? 'grid-outline'
+              : 'ios-grid-outline'
+          } 
+         
+          else if (route.name === 'CreatePosts')
+          {
+            iconName = focused ? 'add-circle' : 'ios-add-circle-outline';
+          }
+           else if( route.name === 'Profile') {
+            iconName = focused ? 'ios-person' : 'ios-person-outline';
           }
 
           // You can return any component that you like here!
-          return <Ionicons name={iconName} size={size} color={color} />;
+          return <Ionicons name={iconName} size={40} color={color} />;
         },
         tabBarActiveTintColor: 'tomato',
         tabBarInactiveTintColor: 'gray',
-        // headerShown: false,
+
       })}
       >
           <HomeTab.Screen name="PostScreen" component={PostsScreen}
@@ -55,22 +63,55 @@ export default function Home() {
               fontSize: 20,
             },
             headerRight: () => (
-              <Button
-                    onPress={() =>
-                        navigation.popToTop()}
-                    title=''
-                    headerBackImageSource={Logout}
-                    // width= '130'
-                    
-                backgroundColor='#000'
-                // color="#fff"
+              <MaterialIcons 
+                name="logout"
+                size={24}
+                color="#BDBDBD"
+                marginRight = {10}              
+                onPress={() =>
+                navigation.popToTop()}
               />
             ),
           }}
           />
+      <HomeTab.Screen name="CreatePosts" component={CreatePostsScreen}
+        options={{
+          title: "Cтворити публікацію",
+          headerTitleAlign: "center",
+          headerStyle: {
+            backgroundColor: "#FFFFFF",
+          },
+            
+          headerTintColor: "#000",
+          headerTitleStyle: {
+            fontWeight: "bold",
+            fontSize: 20,
+          },
+          headerLeft: () => (
+              <MaterialIcons 
+                name="arrow-back"
+                size={24}
+                color="#212121"
+                marginLeft = {10}              
+                onPress={() =>
+                navigation.navigate("Profile")}
+              />
+            ),
+        }}
+      />
       <HomeTab.Screen name="Profile" component={Profile} />
-      <HomeTab.Screen name="CreatePosts" component={CreatePostsScreen} />
-      <HomeTab.Screen name="Comments" component={CommentsScreen} />
+      {/* <HomeTab.Screen name="Comments" component={CommentsScreen} /> */}
     </HomeTab.Navigator>
 )
 };
+
+const styles = StyleSheet.create({
+  container: {
+    fontFamily: 'Roboto',
+    fontSize: 16,
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
